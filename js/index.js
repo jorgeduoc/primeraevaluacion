5bb7425276368ba75b54429193764af828bfd5b2
jorgeduoc <jorg.medel@duocuc.cl>
2021-06-05 19:41:37 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 
 //obtenedor de ubicacion al cargar la pag
+ 
 const fetchData = position =>
-{
-    const {latitude, longitude} =position.coords;
-    const url = "https://www.metaweather.com/api/location/search/?lattlong="
-   
-    fetch(`${url}${latitude},${longitude}`)
-    .then(Response => response.json())
-    .then(data => console.log(data));
+    {
+        const url = "https://www.metaweather.com/api/location/search/?lattlong="
+        const {latitude, longitude} =position.coords;
+        const newurl = "https://www.metaweather.com/api/location/"
         
-        
-
+        var urlformatted = `${url}${latitude},${longitude}`;
+        var newUrl =`https://www.metaweather.com/api/location/${data[0].woeid}`;
+                      console.log(newUrl);
+       
+        fetch(`https://www.metaweather.com/api/location/search/?lattlong=${latitude},${longitude}`)
+        .then(Response => Response.json())
+        .then(data => {
+            let element = document.getElementById('elem')
+            element.innerHTML =  `<p>${data.title} </p>`
+            
+            console.log(`La temperatura promedio es ${data.consolidated_weather[0].the_temp}°C`)})
+    
+        .catch(err=> console.log())
 
 
     console.log(position)
